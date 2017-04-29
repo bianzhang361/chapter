61fe9a9a8b1c9298d57a542603275a6411835853
bianzhang361 <bianzhang361@163.com>
2017-04-29 11:38:53 +0800

    Initial commit

---
+++ chapter1/src/main/webapp/WEB-INF/jsp/main.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2017/4/12
+  Time: 14:39
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html>
+    <head>
+        <title>景区后台管理主页面</title>
+    </head>
+    <body>
+        ${user.userName}，欢迎您进入景区后台管理！
+    </body>
+</html>
---
+++ chapter2/src/main/java/com/smart/beanfactory/BeanFactoryTest.java
@@ -0,0 +1,21 @@
+package com.smart.beanfactory;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
+
+/**
+ * Created by HP on 2017/4/16.
+ */
+public class BeanFactoryTest {
+    public static void main(String[] args) {
+        ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        Resource res = resolver.getResource("classpath:com/smart/beanfactory/beans.xml");
+        BeanFactory bf = new XmlBeanFactory(res);
+        System.out.println("init BeanFactory.");
+        Car car = bf.getBean("car", Car.class);
+        System.out.println("car bean is ready for use!");
+    }
+}
