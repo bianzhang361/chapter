00c4d0f9217873c3403cf06d840ea5ba0180ccbe
Administrator <abc@abc.com>
2017-05-02 15:21:19 +0800

    add Car class.

--- chapter2/src/main/java/com/smart/beanfactory/BeanFactoryTest.java
+++ chapter2/src/main/java/com/smart/beanfactory/BeanFactoryTest.java
@@ -1,5 +1,6 @@
 package com.smart.beanfactory;
 
+import com.smart.Car;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.Resource;
